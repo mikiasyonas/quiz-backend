5cf83a7946b7410b2c2b08f7d8e18ddfbfb18ed7
mikiasyonas <mikiasyonas512@gmail.com>
2021-07-01 21:17:22 +0300

    mongodb fix

--- config/db.js
+++ config/db.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  const MONGO_URI = "mongodb+srv://admin:vzzGN4cZQD4WGZnA@cluster0.fn8gb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  // const MONGO_URI = "mongodb+srv://admin:vzzGN4cZQD4WGZnA@cluster0.fn8gb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
   const MONGO_URI_LOCAL="mongodb://localhost/quizManagement";
-  const conn = await mongoose.connect(MONGO_URI_LOCAL, {
+  MONGO_URI="mongodb+srv://dreamer512:Mikias.Yonas@quiz-management.iqi6d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  const conn = await mongoose.connect(MONGO_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,

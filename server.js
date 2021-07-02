12b4b79e6be10562fc78448a53ebc44dbb459bc1
mikiasyonas <mikiasyonas512@gmail.com>
2021-07-02 09:03:08 +0300

    cors again

--- server.js
+++ server.js
@@ -26,18 +26,6 @@ const app = express();
 app.use(methodOverride("_method"));
 // Route files
 
-//enable cors for vercel
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-  next();
-});
-
-//end cors
-
 // Swagger
 const swaggerOptions = {
   swaggerDefinition: {
